e6cc5e6ccbdf1ddb1f1516a8e0279827ac866267
Kevin Hong <kevin@dnastack.com>
2025-10-22 12:17:54 -0400

    [CU-86b7579tq] fix schema to support hook modifiers in all steps
    Co-authored-by: Jonathan Fuerth <jonathan@dnastack.com>

--- main.go
+++ main.go
@@ -14,7 +14,9 @@ func stepSchema(schema *jsonschema.Schema) {
 
 	// thx: https://gist.github.com/4662752c4af75b3220d94b657683d090
 	// https://concourse-ci.org/steps.html#schema.step
-	stepConfigs := []atc.StepConfig{
+
+	// Core step types that can appear in the anyOf
+	coreStepConfigs := []atc.StepConfig{
 		&atc.GetStep{},
 		&atc.PutStep{},
 		&atc.TaskStep{},
@@ -25,7 +27,20 @@ func stepSchema(schema *jsonschema.Schema) {
 		&atc.TryStep{},
 	}
 
-	for _, s := range stepConfigs {
+	// Hook and modifier properties that can be added to any step
+	hookAndModifierConfigs := []atc.StepConfig{
+		&atc.OnSuccessStep{},
+		&atc.OnFailureStep{},
+		&atc.OnAbortStep{},
+		&atc.OnErrorStep{},
+		&atc.EnsureStep{},
+		&atc.TimeoutStep{},
+		&atc.RetryStep{},
+		&atc.AcrossStep{},
+	}
+
+	// Add core step types to anyOf
+	for _, s := range coreStepConfigs {
 		stepSchema := jsonschema.Reflect(s)
 		stepDef.AnyOf = append(stepDef.AnyOf, stepSchema.Type)
 		for subName, subDef := range stepSchema.Definitions {
@@ -34,6 +49,40 @@ func stepSchema(schema *jsonschema.Schema) {
 			}
 		}
 	}
+
+	// Reflect hook and modifier types to get their definitions
+	for _, s := range hookAndModifierConfigs {
+		stepSchema := jsonschema.Reflect(s)
+		for subName, subDef := range stepSchema.Definitions {
+			if _, present := schema.Definitions[subName]; !present {
+				schema.Definitions[subName] = subDef
+			}
+		}
+	}
+
+	// Add hook and modifier properties to each core step type
+	// These hooks can be applied to any step type
+	hookProperties := map[string]*jsonschema.Type{
+		"on_success": {Ref: "#/definitions/Step"},
+		"on_failure": {Ref: "#/definitions/Step"},
+		"on_abort":   {Ref: "#/definitions/Step"},
+		"on_error":   {Ref: "#/definitions/Step"},
+		"ensure":     {Ref: "#/definitions/Step"},
+		"timeout":    {Type: "string"},
+		"attempts":   {Type: "integer"},
+		"across":     {Type: "array", Items: &jsonschema.Type{Ref: "#/definitions/AcrossVarConfig"}},
+		"fail_fast":  {Type: "boolean"},
+	}
+
+	// Add hook properties to each core step definition
+	for _, stepName := range []string{"GetStep", "PutStep", "TaskStep", "SetPipelineStep", "LoadVarStep", "InParallelStep", "DoStep", "TryStep"} {
+		if stepTypeDef, exists := schema.Definitions[stepName]; exists {
+			for hookName, hookType := range hookProperties {
+				stepTypeDef.Properties.Set(hookName, hookType)
+			}
+		}
+	}
+
 	stepDef.Required = nil
 	schema.Definitions["Step"].Properties = nil
 	schema.Definitions["Step"].Type = ""
